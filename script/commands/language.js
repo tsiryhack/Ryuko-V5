71e00792d50866211c12bd53642577e722aa0b50
tsiryhack <anjaratovomilay@gmail.com>
2026-08-22 22:08:06 +0300

    Update language.js

--- script/commands/language.js
+++ script/commands/language.js
@@ -1,41 +1,41 @@
 module.exports.config = {
-	name: "language",
-	version: "1.0.0",
-	permission: 2,
-	prefix: true,
-	credits: "ryuko",
-	description: "change the bot language",
-  premium: false,
-	category: "admin",
-	usages: "[bangla] [english]",
-	cooldowns: 5
+        name: "language",
+        version: "1.0.0",
+        permission: 2,
+        prefix: true,
+        credits: "ryuko",
+        description: "Changer la langue du bot",
+        premium: false,
+        category: "admin",
+        usages: "[français] [english]",
+        cooldowns: 5
 };
 
-module.exports.run = async ({ api, event, args, getText }) => {
-  let operator = global.config.operators;
-            if (!operator.includes(event.senderID)) return api.sendMessage(`only bot operators can use this command.`, event.threadID, event.messageID);
+module.exports.run = async ({ api, event, args }) => {
+    let operator = global.config.operators;
+    if (!operator.includes(event.senderID)) return api.sendMessage(`Seuls les administrateurs peuvent utiliser cette commande.`, event.threadID, event.messageID);
     const { threadID, messageID } = event;
 
     switch (args[0]) {
-        case "bangla":
-        case "bangla":
+        case "français":
+        case "francais":
+        case "fr":
             {
-                return api.sendMessage(`ngôn ngữ đã được chuyển sang tiếng việt`, threadID, () => global.config.language = "bangla"); 
+                return api.sendMessage(`Le bot est déjà configuré en français !`, threadID, () => global.config.language = "fr"); 
             }
             break;
-        
+
         case "english":
-        case "english":
+        case "en":
             {
-                return api.sendMessage(`language has been converted to english`, threadID, () => global.config.language = "english"); 
+                return api.sendMessage(`Language has been converted to english`, threadID, () => global.config.language = "english"); 
             }
             break;
-    
+
         default:
             {
-                return api.sendMessage("syntax error, use : language bangla/english", threadID, messageID);
+                return api.sendMessage("Erreur de syntaxe. Utilisez : language fr ou language english", threadID, messageID);
             }   
             break; 
-            
-    }	
+    }        
 }
